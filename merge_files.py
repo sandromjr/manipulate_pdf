71dea25261f7e97061027e36aea9bd20820c0f25
Sandro Junior <60121783+sandromjr@users.noreply.github.com>
2020-08-06 12:16:39 -0300

    Update merge_files.py

--- merge_files.py
+++ merge_files.py
@@ -4,21 +4,28 @@ def merge_pdf():
 
     pdf2merge = [file1, file2]
     pdfWriter = PdfFileWriter()
+    
     # loop through all PDFs
     for filename in pdf2merge:
+        
         # rb for read binary
         pdfFileObj = open(filename, 'rb')
         pdfReader = PdfFileReader(pdfFileObj)
+        
         # Opening each page of the PDF
         for pageNum in range(pdfReader.numPages):
             pageObj = pdfReader.getPage(pageNum)
             pdfWriter.addPage(pageObj)
+            
     # save PDF to file, wb for write binary
     pdfOutput = open(merged_file, 'wb')
+    
     # Outputting the PDF
     pdfWriter.write(pdfOutput)
+    
     # Closing the PDF writer
     pdfOutput.close()
+    
     return('Files merged')
 
 #files to be merged
